6fd5b513e5a759927adf8d3934051ec38e12fc50
mgtezak <mgtezak@gmail.com>
2023-11-01 22:56:21 +0100

    update

--- tabs/about_tab.py
+++ tabs/about_tab.py
@@ -10,13 +10,13 @@ def run():
         using your favorite programming language, I highly recommend you check it out!
 
         Every year over the course of 25 days starting on December 1st, the creator [Eric Wastl](http://was.tl/) uploads daily coding challenges.
-        Each challenge has two parts, the first one oftentimes being a lot easier and serving as a warm-up for the second one. 
+        Each challenge has two parts, the first one is oftentimes a lot easier and serves as a warm-up for the second one. 
         A cool thing about these challenges is that everyone gets their own individual puzzle input, such that everyone's puzzle answer 
         will be different as well. 
 
         If you get stuck there's a huge community on [reddit](https://www.reddit.com/r/adventofcode/), 
         youtube and various other platforms, of people sharing their approaches to each problem.
-        I learned a ton about programming in python by trying to solve these problems and then going online and and comparing my solution to those of others.
+        I learned a ton about programming in python by trying to solve these problems and then going online and comparing my solution to those of others.
         With this website I'd like to contribute to this community, by sharing my own code and creating a fun interface to play around with.
 
         That being said, I put this project together in a very short time and it is still a work in progress that I plan to build upon 
