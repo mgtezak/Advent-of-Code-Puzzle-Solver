aa9fce91416d1c6027e618da067cdd1d153f2b6c
mgtezak <mgtezak@gmail.com>
2023-11-05 21:56:08 +0100

    update

--- my_functions/solutions/aoc2021.py
+++ my_functions/solutions/aoc2021.py
@@ -1,6 +1,7 @@
 import re
 import math
 from collections import defaultdict, Counter
+import heapq
 
 
 def aoc2021_day1_part1(puzzle_input):
@@ -763,10 +764,64 @@ def aoc2021_day14_part2(puzzle_input):
 
 
 def aoc2021_day15_part1(puzzle_input):
-    pass
+    grid = [[int(i) for i in row] for row in puzzle_input.split()]
+    upper = len(grid) - 1
+    visited = set()
+    heap = [(0, 0, 0)]
+    while heap:
+        total, x, y = heapq.heappop(heap)
+        if (x, y) in visited:
+            continue
+        if x == y == upper:
+            break
+        if x > 0:
+            heapq.heappush(heap, (total + grid[x-1][y], x-1, y))
+        if x < upper:
+            heapq.heappush(heap, (total + grid[x+1][y], x+1, y))
+        if y > 0:
+            heapq.heappush(heap, (total + grid[x][y-1], x, y-1))
+        if y < upper:
+            heapq.heappush(heap, (total + grid[x][y+1], x, y+1))
+        visited.add((x, y))
+
+    return total
+
 
 def aoc2021_day15_part2(puzzle_input):
-    pass
+    grid = [[int(i) for i in row] for row in puzzle_input.split()]
+    n = len(grid)
+
+    # Expand width
+    for r in range(n):
+        for s in range(1, 5):
+            grid[r].extend([(i - 1 + s) % 9 + 1 for i in grid[r][:n]])
+
+    # Expand length
+    for s in range(1, 5):
+        for r in range(n):
+            grid.append([(i - 1 + s) % 9 + 1 for i in grid[r]])
+
+    upper = len(grid) - 1
+    visited = set()
+    heap = [(0, 0, 0)]
+    while heap:
+        total, x, y = heapq.heappop(heap)
+        if (x, y) in visited:
+            continue
+        if x == y == upper:
+            break
+        if x > 0:
+            heapq.heappush(heap, (total + grid[x-1][y], x-1, y))
+        if x < upper:
+            heapq.heappush(heap, (total + grid[x+1][y], x+1, y))
+        if y > 0:
+            heapq.heappush(heap, (total + grid[x][y-1], x, y-1))
+        if y < upper:
+            heapq.heappush(heap, (total + grid[x][y+1], x, y+1))
+        visited.add((x, y))
+
+    return total
+
 
 ########################################################################################
 
