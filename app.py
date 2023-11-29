ef0c0a6c970a9ff165a8fce62b0dfabd74adda6c
mgtezak <mgtezak@gmail.com>
2023-11-30 00:28:53 +0100

    fix bug

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def run():
     st.sidebar.markdown('''
         <span style="font-size: 0.9em;">Links:</span>   
         <a href="https://mgtezak.github.io" style="color: #FFD700; font-weight: Normal;"> ~ My Website</a>  
-        <a href="https://github.com/mgtezak" style="color: #FFD700; font-weight: Normal;"> ~ My Github</a>
+        <a href="https://github.com/mgtezak" style="color: #FFD700; font-weight: Normal;"> ~ My Github</a>  
         <a href="https://mgtezak-data-science.streamlit.app" style="color: #FFD700; font-weight: Normal;"> ~ My Data Science Projects</a>
     ''', unsafe_allow_html=True)
 
