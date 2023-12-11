0066e80505b514f551b7e654c675244823fbf90d
mgtezak <mgtezak@gmail.com>
2023-12-11 19:38:50 +0100

    update

--- lib/solutions/aoc2023.py
+++ lib/solutions/aoc2023.py
@@ -1,7 +1,7 @@
 import re
 import math
 from functools import cmp_to_key
-from itertools import cycle
+from itertools import cycle, combinations
 from collections import Counter
 
 
@@ -424,11 +424,53 @@ def aoc2023_day10_part2(puzzle_input):
 
 
 def aoc2023_day11_part1(puzzle_input):
-    pass
+    grid = [[ele for ele in line] for line in puzzle_input.split('\n')]
+    m, n = len(grid), len(grid[0])
+
+    i = 0
+    while i < len(grid):
+        if '#' not in grid[i]:
+            grid.insert(i, ['.'] * n)
+            m += 1
+            i += 2
+        else:
+            i += 1
+
+    j = 0
+    while j < len(grid[0]):
+        if '#' not in [grid[i][j] for i in range(m)]:
+            for i in range(m):
+                grid[i].insert(j, '.')
+            n += 1
+            j += 2
+        else:
+            j += 1
+
+    galaxies = [(i, j) for i in range(m) for j in range(n) if grid[i][j] == '#']
+    total = 0
+    for (x1, y1), (x2, y2) in combinations(galaxies, 2):
+        total += abs(x1-x2) + abs(y1-y2)
+    
+    return total
 
 
 def aoc2023_day11_part2(puzzle_input):
-    pass
+    grid = [[ele for ele in line] for line in puzzle_input.split('\n')]
+    m, n = len(grid), len(grid[0])
+
+    empty_rows = [i for i in range(m) if '#' not in grid[i]]
+    empty_cols = [j for j in range(n) if all(grid[i][j]=='.' for i in range(m))]
+    galaxies = [(i, j) for i in range(m) for j in range(n) if grid[i][j] == '#']
+    
+    total = 0
+    for (x1, y1), (x2, y2) in combinations(galaxies, 2):
+        x1, x2 = sorted((x1, x2))
+        y1, y2 = sorted((y1, y2))
+        add_rows = 999_999 * sum(r in range(x1, x2) for r in empty_rows)
+        add_cols = 999_999 * sum(c in range(y1, y2) for c in empty_cols)
+        total += x2 - x1 + add_rows + y2 - y1 + add_cols
+
+    return total
 
 
 ####################################################################################################
