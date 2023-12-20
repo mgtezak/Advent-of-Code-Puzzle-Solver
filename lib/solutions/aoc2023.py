f397e82c34dff661a68cd95ee437cf7e23be2eed
mgtezak <mgtezak@gmail.com>
2023-12-20 11:57:09 +0100

    add day 19

--- lib/solutions/aoc2023.py
+++ lib/solutions/aoc2023.py
@@ -4,6 +4,7 @@ from functools import cmp_to_key, cache
 from itertools import cycle, combinations
 from collections import Counter, defaultdict
 from heapq import heappop, heappush
+import operator
 
 import numpy as np
 
@@ -1072,11 +1073,85 @@ def aoc2023_day18_part2(puzzle_input):
 
 
 def aoc2023_day19_part1(puzzle_input):
-    pass
+    workflows, parts = puzzle_input.split('\n\n')
+    work_regex = r'(\w+)\{([^}]+)\}'
+    cond_regex = r'(\w+)(<|>)(\d+):(\w+)'
+    flow = {}
+    for name, rules in re.findall(work_regex, workflows):
+        conditional = re.findall(cond_regex, rules)
+        final = rules.split(',')[-1]
+        flow[name] = conditional + [final]
+
+    part_regex = r'x=(\d+),m=(\d+),a=(\d+),s=(\d+)'
+    comp = {'>': operator.gt, '<': operator.lt}
+    total_accepted = 0
+    for part in re.findall(part_regex, parts):
+        part = dict(zip('xmas', map(int, part)))
+        curr = 'in'
+        while curr not in ('A', 'R'):
+            for cat, op, amt, res in flow[curr][:-1]:
+                if comp[op](part[cat], int(amt)):
+                    curr = res
+                    break
+            else:
+                curr = flow[curr][-1]
+
+        if curr == 'A':
+            total_accepted += sum(part.values())
+
+    return total_accepted
 
 
 def aoc2023_day19_part2(puzzle_input):
-    pass
+    workflows, parts = puzzle_input.split('\n\n')
+    work_regex = r'(\w+)\{([^}]+)\}'
+    cond_regex = r'(\w+)(<|>)(\d+):(\w+)'
+    flow = {}
+    for name, rules in re.findall(work_regex, workflows):
+        conditional = []
+        for cat, op, amt, res in re.findall(cond_regex, rules):
+            conditional.append(('xmas'.index(cat), op, int(amt), res))
+        final = rules.split(',')[-1]
+        flow[name] = conditional + [final]
+
+    start = ('in', (1, 4000), (1, 4000), (1, 4000), (1, 4000))
+    queue = [start]
+    total_accepted = 0
+    while queue:
+        curr, *intervals = queue.pop()
+        if curr in ('A', 'R'):
+            if curr == 'A':
+                total_accepted += math.prod(hi-lo+1 for lo, hi in intervals)
+            continue
+
+        for cat_idx, op, amt, res in flow[curr][:-1]:
+            lo, hi = intervals[cat_idx]
+
+            # All passthrough, no transfer
+            if (op == '>' and amt >= hi) or (op == '<' and amt <= lo):
+                continue
+
+            # All transfer no passthrough
+            if (op == '>' and amt < lo) or (op == '<' and amt > hi):
+                queue.append((res, *intervals))
+                break
+
+            # Some of both
+            if op == '>':
+                transfer = (amt+1, hi)
+                passthrough = (lo, amt)
+            else:
+                transfer = (lo, amt-1)
+                passthrough = (amt, hi)
+            intervals[cat_idx] = passthrough
+            intervals2 = intervals.copy()
+            intervals2[cat_idx] = transfer
+            queue.append((res, *intervals2))
+        
+        else: # Remaining is transferred
+            queue.append((flow[curr][-1], *intervals))
+
+    return total_accepted
 
 
 ####################################################################################################
